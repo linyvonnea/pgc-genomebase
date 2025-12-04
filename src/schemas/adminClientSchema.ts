e083e2e3fd4510c61e11726f58f71d76c7fdad81
Merlito Dayon Jr <34882626+mdayon39@users.noreply.github.com>
2025-12-04 15:31:04 +0800

    Merlito/populate (#107)
    * fix: allow builder page to load without projectId
    
    * Load page even without prjectID
    
    * copy of builder-page
    
    * test
    
    * test2
    
    * backup copy
    
    * Comment out affiliationAddress
    
    * Change haveSubmitted and isContactPerson to false
    
    * Change haveSubmitted and isContactPerson to null
    
    * change from false to null
    
    * Comment out affiliationAddress
    
    * Comment affliation address
    
    * remove affiliation address
    
    * remove affiliation address form field
    
    * remove affiliation address form field
    
    * Comment affiliation address

--- src/schemas/adminClientSchema.ts
+++ src/schemas/adminClientSchema.ts
@@ -8,7 +8,7 @@ export const adminClientSchema = z.object({
   designation: z.string().min(1, "Designation is required"),
   sex: z.enum(["F", "M", "Other"]),
   phoneNumber: z.string().regex(/^\d{11}$/, "Enter a valid 11-digit number with no spaces"),
-  affiliationAddress: z.string().min(1, "Address is required"),
+  //affiliationAddress: z.string().min(1, "Address is required"),
 });
 
 export type AdminClientData = z.infer<typeof adminClientSchema>;
