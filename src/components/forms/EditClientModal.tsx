96b8e35ed6ee66d61410cb99bbb481a1f5c3beac
Merlito Jr Dayon <merlito.dayon@gmail.com>
2025-12-04 14:46:22 +0800

    Comment affliation address

--- src/components/forms/EditClientModal.tsx
+++ src/components/forms/EditClientModal.tsx
@@ -61,6 +61,7 @@ export function EditClientModal({ client, onSuccess }: EditClientModalProps) {
     },
   });
 
+  
   const onSubmit = async (data: AdminClientData) => {
     setIsLoading(true);
     try {
