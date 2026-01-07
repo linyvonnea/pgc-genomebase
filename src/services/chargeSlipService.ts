e5d21051db82ceeabbcc3c1c2be773c7545361af
Merlito Jr Dayon <merlito.dayon@gmail.com>
2026-01-07 11:51:04 +0800

    comment orderBy dateissued desc

--- src/services/chargeSlipService.ts
+++ src/services/chargeSlipService.ts
@@ -157,7 +157,7 @@ export async function getChargeSlipsByProjectId(projectId: string): Promise<Char
     query(
       collection(db, CHARGE_SLIPS_COLLECTION),
       where("projectId", "==", projectId),
-      orderBy("chargeSlipNumber", "desc")
+      //orderBy("dateIssued", "desc")
     )
   );
 
