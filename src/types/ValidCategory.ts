57376b91a3f3b11a5b99ccc4221260d0be76f9c9
Merlito Jr Dayon <merlito.dayon@gmail.com>
2026-01-29 23:09:26 +0800

    add sorting checkboxes on charge slip section

--- src/types/ValidCategory.ts
+++ src/types/ValidCategory.ts
@@ -1,2 +1,2 @@
 // src/types/ValidCategory.ts
-export type ValidCategory = "laboratory" | "equipment" | "bioinformatics" | "retail";
+export type ValidCategory = "laboratory" | "equipment" | "bioinformatics" | "retail" | "training";
