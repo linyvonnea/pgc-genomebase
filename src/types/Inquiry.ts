a2fbcc48b6ffca950873bb297d3ce58c0cc0b866
Merlito Dayon Jr <34882626+mdayon39@users.noreply.github.com>
2026-04-20 11:52:55 +0800

    Revert "Fix/vercel or entries type (#1824)" (#1825)
    This reverts commit c092a1626bc26efe2192c4c2dfc62fcb73eb1a61.

--- src/types/Inquiry.ts
+++ src/types/Inquiry.ts
@@ -3,7 +3,7 @@ export interface Inquiry {
   createdAt: Date;
   name: string;
   isApproved: boolean;
-  status: 'Pending' | 'Ongoing Quotation' | 'Approved Client' | 'Quotation Only' | 'Service Not Offered' | 'Cancelled' | 'In Progress';
+  status: 'Pending' | 'Ongoing Quotation' | 'Approved Client' | 'Quotation Only' | 'Service Not Offered' | 'Cancelled';
   affiliation: string;
   designation: string;
   email?: string;
